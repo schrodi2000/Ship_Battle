0fa1ed1a0e1f53dfa028d57fb010b575f7bf4ba0
schrodi2000 <schrodi2000@gmail.com>
2021-01-16 12:40:01 +0100

    7x7 raster erstellt in grafik

--- src/com/schrodi/Main.java
+++ src/com/schrodi/Main.java
@@ -1,17 +1,7 @@
 package com.schrodi;
 
 public class Main {
-
     public static void main(String[] args) {
-        Window fenster = new Window(1000,500);
-
-    }
-
-
-    void map(int[] size){
-        //todo
-    }
-    boolean shot(int[] coordinate){
-        return true; //todo
+        Window fenster = new Window(7,"Schiffe Versenken",1000,500);
     }
 }
--- src/com/schrodi/Window.java
+++ src/com/schrodi/Window.java
@@ -1,20 +1,26 @@
 package com.schrodi;
 import processing.core.PApplet;
 public class Window extends PApplet{
-    int x,y;
-    public Window(int x,int y){
-        this.x = x;
-        this.y = y;
-        String[] processingArgs = {"title"};
+    private float sizeX, sizeY,field;
+    public Window(int field, String title, int sizeX, int sizeY){
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.field = field;
+        String[] processingArgs = {title};
         PApplet.runSketch(processingArgs, this);
     }
     @Override
     public void settings() {
-        size(x,y);
+        size((int)sizeX, (int)sizeY);
     }
     @Override
     public void draw() {
-        background(140);
+        rectMode(RADIUS);
+        for(int i=1; i<=field; i++){
+            for(int m=1; m<=field; m++){
+                rect(i*(sizeX /field), m*(sizeY /field), sizeX /field, sizeY /field);
+            }
+        }
     }
     @Override
     public void mousePressed() {
