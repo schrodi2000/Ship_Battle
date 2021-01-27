4dc25065ee0bb9897a8abd6ba6f304138565d7db
schrodi2000 <schrodi2000@gmail.com>
2021-01-27 14:40:24 +0100

    Die grafik gemacht und in der Grafik auch die randbezeichnungen abc 123 gemacht.

--- src/com/schrodi/Window.java
+++ src/com/schrodi/Window.java
@@ -7,11 +7,36 @@ public class Window {
         this.mapSize = mapSize;
     }
 
-    public void draw() {
-        for (int ix = 0; ix < mapSize; ix++) {
-            for (int iy = 0; iy < mapSize; iy++) {
-                // TODO grafik der dinger ausgeben:)
-            }
+    public void draw(Map map) {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.print("  ");
+        for(int i = 1; i<=mapSize;i++){
+
+            System.out.print(i);
         }
+        System.out.println("");
+        for (int iy = 0; iy < mapSize; iy++) {
+            Character buchstabe = alphabet.charAt(iy);
+            System.out.print(buchstabe + " ");
+            for (int ix = 0; ix < mapSize; ix++) {
+                switch (map.getMapKarteTeil(ix,iy)){
+
+                    case aliveShip -> {
+                        System.out.print('#');
+                    }
+                    case deadShip -> {
+                        System.out.print('X');
+                    }
+                    case water -> {
+                        System.out.print('-');
+                    }
+                    case miss -> {
+                        System.out.print('~');
+                    }
+                }
+            }
+            System.out.println("");
+        }
+        System.out.println("");
     }
 }
