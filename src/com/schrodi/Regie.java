198d3eb5c4f7d834027f0849fd58b6d997436804
schrodi2000 <schrodi2000@gmail.com>
2021-02-17 23:32:09 +0100

    Grafik mit Processing angefangen und Processing importiert

--- src/com/schrodi/Regie.java
+++ src/com/schrodi/Regie.java
@@ -1,7 +1,6 @@
 package com.schrodi;
 
-public class Output {
-    int mapSize;
+import processing.core.PApplet;
 
     public Output(int mapSize) {
         this.mapSize = mapSize;
@@ -11,31 +10,76 @@ public class Output {
         System.out.print("  ");
         for(int i = 1; i<=mapSize;i++){
 
-            System.out.print(" " + i);
-        }
-        System.out.println("");
-        for (int iy = 0; iy < mapSize; iy++) {
-            char buchstabe = (char) ('A' + iy);
-            System.out.print(buchstabe + " ");
-            for (int ix = 0; ix < mapSize; ix++) {
-                switch (player.getMapKarteTeil(ix,iy)){
+    public Regie(Player s, Player g) {
+        this.s = s;
+        this.g = g;
+        seitenVerhaeltniss = 16/9f;
+        width = 1920;
+        height = (int) (width / seitenVerhaeltniss);
+        spielStatus = GameState.schiffeSetzen;
+        String[] processingArgs = {"Schiffe-versenken"};
+        PApplet.runSketch(processingArgs, this);
+    }
 
-                    case aliveShip -> {
-                        System.out.print('#');
-                    }
-                    case deadShip -> {
-                        System.out.print('X');
-                    }
-                    case water -> {
-                        System.out.print('-');
-                    }
-                    case miss -> {
-                        System.out.print('~');
-                    }
-                }
+    public void settings() {
+        size(width, height);
+    }
+
+    public void setup(){
+        surface.setResizable(true);
+    }
+
+    public void draw() {
+        seitenVerhaeltniss = (float)width/(float)height; // Seitenverhältnis aktualisieren
+        drawWindow(s, g);
+    }
+
+    void seitenverhaeltnissBeibehalten(){
+        width = width;
+        surface.setSize(width, (int) (width * 0.5625));
+    }
+
+    void drawWindow(Player spieler, Player gegner) {
+        switch (spielStatus) {
+            case schiffeSetzen: {
+                drawMap(spieler, false);
+                drawMap(gegner, true);
+
+                drawSchiffeliste();
             }
-            System.out.println("");
         }
-        System.out.println("");
+    }
+
+    void drawMap(Player player, Boolean isEnemy) {
+        int mapPosX;
+        float tileHeight = height *seitenVerhaeltniss*(30/700f);
+        float tileWidth = width *(30/700f);
+        int mapPosY = (int)(width *0.10);
+        if (isEnemy) {
+            mapPosX = (int)(width *0.60);
+        } else {
+            mapPosX = (int)(width *0.10);
+            mapPosY = mapPosX;
+        }
+        for(int iy = 0; iy<7; iy++){
+            for(int jx = 0; jx<7; jx++){
+                switch(player.getMapKarteTeil(iy,jx)){
+                    case water -> fill(0,0,255);
+                    case miss -> fill(122,122,255);
+                    case deadShip -> fill(255,0,0);
+                    case aliveShip -> fill(0,255,0);
+                }
+                rect(mapPosX+jx*tileWidth,mapPosY+iy*tileHeight, (int)tileWidth-10, (int)tileHeight-10);
+            }
+        }
+
+    }
+
+    void drawSchiffeliste() {
+
+    }
+
+    void addInfo() {
+
     }
 }
