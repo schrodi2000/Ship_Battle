97e497e6ebe486cafbe5f52b895508985d94f762
schrodi2000 <schrodi2000@gmail.com>
2021-02-18 16:17:57 +0100

    Die Layout Größen sind nun als Variablen festgelegt und werden Aktualisiert. Außerdem habe ich die Variablen mit den werten in den Funktionen ersetzt.

--- src/com/schrodi/Regie.java
+++ src/com/schrodi/Regie.java
@@ -14,18 +14,34 @@ public class Regie extends PApplet {
         spielEnde
     }
 
-    GameState spielStatus;
+    public GameState spielStatus;
     float seitenVerhaeltniss;
     String[] verlauf = new String[10];
-    Player s,g;
+    Player s, g;
+    // Layout größen Design
+    float rand;
+    float tileSize;
+    float tileVerkleinerungFaktor;
+    float infobereich;
+    float infobereichAbstandTiles;
+    float spielfeldGegner;
+    int mapSize = 3;
+
 
     public Regie(Player s, Player g) {
         this.s = s;
         this.g = g;
-        seitenVerhaeltniss = 16/9f;
         width = 1920;
-        height = (int) (width / seitenVerhaeltniss);
+        height = 1080;
+        seitenVerhaeltniss = 16 / 9f; // das f steht dafür, dass java daraus keine int division macht sondern eine float division
         spielStatus = GameState.schiffeSetzen;
+        rand = width * 0.1f;
+        tileSize = (width - 2 * rand - 4 * infobereichAbstandTiles) / (mapSize*2+3);
+        tileVerkleinerungFaktor = 0.9f;
+        infobereich = rand + tileSize * mapSize;
+        infobereichAbstandTiles = (tileSize * 0.9f - tileSize)*-1;
+        spielfeldGegner = infobereich + 3 * tileSize + 4 * infobereichAbstandTiles;
+
         String[] processingArgs = {"Schiffe-versenken"};
         PApplet.runSketch(processingArgs, this);
     }
@@ -34,7 +50,7 @@ public class Regie extends PApplet {
         size(width, height);
     }
 
-    public void setup(){
+    public void setup() {
         surface.setResizable(true);
     }
 
@@ -43,9 +59,15 @@ public class Regie extends PApplet {
         drawWindow(s, g);
     }
 
-    void seitenverhaeltnissBeibehalten(){
-        width = width;
-        surface.setSize(width, (int) (width * 0.5625));
+    void layoutGrößeAktualisieren() {
+        seitenVerhaeltniss = 16 / 9f; // das f steht dafür, dass java daraus keine int division macht sondern eine float division
+        spielStatus = GameState.schiffeSetzen;
+        rand = width * 0.1f;
+        tileSize = (width - 2 * rand - 4 * infobereichAbstandTiles) / (mapSize*2+3);
+        tileVerkleinerungFaktor = 0.9f;
+        infobereich = rand + tileSize * mapSize;
+        infobereichAbstandTiles = (tileSize * 0.9f - tileSize)*-1;
+        spielfeldGegner = infobereich + 3 * tileSize + 4 * infobereichAbstandTiles;
     }
 
     void drawWindow(Player spieler, Player gegner) {
@@ -60,31 +82,36 @@ public class Regie extends PApplet {
     }
 
     void drawMap(Player player, Boolean isEnemy) {
-        int mapPosX;
-        float tileHeight = height *seitenVerhaeltniss*(30/700f);
-        float tileWidth = width *(30/700f);
-        int mapPosY = (int)(width *0.10);
+        float mapPosX;
         if (isEnemy) {
-            mapPosX = (int)(width *0.60);
+            mapPosX = spielfeldGegner;
         } else {
-            mapPosX = (int)(width *0.10);
-            mapPosY = mapPosX;
+            mapPosX = rand;
         }
-        for(int iy = 0; iy<7; iy++){
-            for(int jx = 0; jx<7; jx++){
-                switch(player.getMapKarteTeil(iy,jx)){
-                    case water -> fill(0,0,255);
-                    case miss -> fill(122,122,255);
-                    case deadShip -> fill(255,0,0);
-                    case aliveShip -> fill(0,255,0);
+        for (int iy = 0; iy < mapSize; iy++) {
+            for (int jx = 0; jx < mapSize; jx++) {
+                switch (player.getMapKarteTeil(iy, jx)) {
+                    case water -> fill(0, 0, 255);
+                    case miss -> fill(122, 122, 255);
+                    case deadShip -> fill(255, 0, 0);
+                    case aliveShip -> fill(0, 255, 0);
                 }
-                rect(mapPosX+jx*tileWidth,mapPosY+iy*tileHeight, (int)tileWidth-10, (int)tileHeight-10);
+                rect(mapPosX + jx * tileSize, rand + iy * tileSize, tileSize * tileVerkleinerungFaktor, tileSize * tileVerkleinerungFaktor);
             }
         }
-
     }
 
-    void drawSchiffeliste() {
+    void drawSchiffeSetzen() {
+        float tilesPosY = rand;
+        float tilesPosX = infobereich + infobereichAbstandTiles;
+        fill(50, 50, 122);
+        for(int i = 3; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                rect(tilesPosX, tilesPosY + j * tileSize, tileSize * tileVerkleinerungFaktor, tileSize * tileVerkleinerungFaktor);
+            }
+            tilesPosX = tilesPosX + tileSize + infobereichAbstandTiles;
+        }
+    }
 
     }
 
