0f82bf20b58d62b68527300acdd72f8c9f5135d2
schrodi2000 <schrodi2000@gmail.com>
2021-02-19 23:19:41 +0100

    addSchiff geändert und addAbleShip erstellt und fertig gemacht. Kleine änderungen.

--- src/com/schrodi/Player.java
+++ src/com/schrodi/Player.java
@@ -2,6 +2,7 @@ package com.schrodi;
 
 import jdk.jshell.spi.ExecutionControl;
 
+import java.util.Random;
 import java.util.ArrayList;
 
 public class Player {
@@ -23,31 +24,95 @@ public class Player {
         schiffe = new ArrayList<>();
         this.mapSize = mapSize;
         mapKarte = new Zustand[mapSize][mapSize];
-        for (int i = 0; i < mapSize; i++) {
-            for (int j = 0; j < mapSize; j++) {
-                mapKarte[i][j] = Zustand.water;
+        for (int ix = 0; ix < mapSize; ix++) {
+            for (int jy = 0; jy < mapSize; jy++) {
+                mapKarte[ix][jy] = Zustand.water;
             }
         }
     }
 
-    public void addSchiff(int x, int y, int length, Schiff.Richtung richtung) {
+    public boolean addSchiff(int x, int y, int length, Schiff.Richtung richtung) {
         Schiff schiff = new Schiff(length, x, y, richtung);// Erstellt ein Schiff mit den eigenschaften
-        schiffe.add(schiff);// fügt das Schiff der Liste hinzu
-        if (schiff.getRichtung() == Schiff.Richtung.horizontal) {
-            for (int i = 0; i < schiff.getLänge(); i++) {// Trägt die Teile des Schiffes ein wenn es Horizontal ausgerichtet ist
-                mapKarte[x + i][y] = schiff.getTeilZustand(i);
+        if (addAbleSchiff(schiff)) {
+            if (schiff.getRichtung() == Schiff.Richtung.horizontal) {
+                for (int i = 0; i < schiff.getLänge(); i++) {// Trägt die Teile des Schiffes ein wenn es Horizontal ausgerichtet ist
+                    mapKarte[x + i][y] = schiff.getTeilZustand(i);
+                }
+            } else {
+                for (int i = 0; i < schiff.getLänge(); i++) {// Trägt die Teile des Schiffes ein wenn es Vertikal ausgerichtet ist
+                    mapKarte[x][y + i] = schiff.getTeilZustand(i);
+                }
             }
+            schiffe.add(schiff);// fügt das Schiff der Liste hinzu
         } else {
-            for (int i = 0; i < schiff.getLänge(); i++) {// Trägt die Teile des Schiffes ein wenn es Vertikal ausgerichtet ist
-                mapKarte[x][y + i] = schiff.getTeilZustand(i);
-            }
+            return false;
         }
+        return true;
     }
 
-    public boolean AnySchiffAlive(){
-        for(Schiff schiff : schiffe){
-            for(int i = 0;i<schiff.getLänge();i++){
-                if(schiff.getTeilZustand(i) == Zustand.aliveShip){
+    boolean addAbleSchiff(Schiff schiff) {
+        try {
+            for (int i = 0; i < schiff.getLänge(); i++) {
+                if (schiff.getRichtung() == Schiff.Richtung.horizontal) {//Horizontale Schiffe
+                    if (mapKarte[schiff.getX()][schiff.getY() + i] == Zustand.aliveShip) {// schiff darf nicht auf anderes schiff gesetzt werden
+                        return false;
+                    }
+                    if (schiff.getX() > 0) {
+                        if (mapKarte[schiff.getX() - 1][schiff.getY()] == Zustand.aliveShip) {// links vom schiff darf kein anderes Schiff stehen
+                            return false;
+                        }
+                    }
+                    if (schiff.getX() + schiff.getLänge() < mapSize) {
+                        if (mapKarte[schiff.getX() + schiff.getLänge()][schiff.getY()] == Zustand.aliveShip) {// rechts vom schiff darf kein anderes Schiff stehen
+                            return false;
+                        }
+                    }
+                    if (schiff.getY() > 0) {
+                        if (mapKarte[schiff.getX() + i][schiff.getY() - 1] == Zustand.aliveShip) {// über dem schiff darf kein anderes Schiff stehen
+                            return false;
+                        }
+                    }
+                    if (schiff.getY() + 1 < mapSize) {
+                        if (mapKarte[schiff.getX() + i][schiff.getY() + 1] == Zustand.aliveShip) {// unter dem schiff darf kein anderes Schiff stehen
+                            return false;
+                        }
+                    }
+                } else {// Vertikale Schiffe
+                    if (mapKarte[schiff.getX()][schiff.getY() + i] == Zustand.aliveShip) {// schiff darf nicht auf anderes schiff gesetzt werden
+                        return false;
+                    }
+                    if (schiff.getX() > 0) {
+                        if (mapKarte[schiff.getX() - 1][schiff.getY() + i] == Zustand.aliveShip) {// links vom schiff darf kein anderes Schiff stehen
+                            return false;
+                        }
+                    }
+                    if (schiff.getX() + 1 < mapSize) {
+                        if (mapKarte[schiff.getX() + 1][schiff.getY() + i] == Zustand.aliveShip) {// rechts vom schiff darf kein anderes Schiff stehen
+                            return false;
+                        }
+                    }
+                    if (schiff.getY() > 0) {
+                        if (mapKarte[schiff.getX()][schiff.getY() - 1] == Zustand.aliveShip) {// über dem schiff darf kein anderes Schiff stehen
+                            return false;
+                        }
+                    }
+                    if (schiff.getY() + schiff.getLänge() < mapSize) {
+                        if (mapKarte[schiff.getX()][schiff.getY() + schiff.getLänge()] == Zustand.aliveShip) {// unter dem schiff darf kein anderes Schiff stehen
+                            return false;
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean AnySchiffAlive() {
+        for (Schiff schiff : schiffe) {
+            for (int i = 0; i < schiff.getLänge(); i++) {
+                if (schiff.getTeilZustand(i) == Zustand.aliveShip) {
                     return true;
                 }
             }
@@ -83,8 +148,29 @@ public class Player {
         return false;
     }
 
+    public boolean randomAddShips(int dreier, int zweier, int einer) {
+        Random random = new Random();
+        int versuche = 0;
+        int i = 0;
+        do {
+            i++;
+            int x = random.nextInt(mapSize);
+            int y = random.nextInt(mapSize);
+            Schiff.Richtung richtung = (random.nextInt(2) == 1) ? Schiff.Richtung.horizontal : Schiff.Richtung.vertikal;
+            if (addSchiff(x, y, 3, richtung) && dreier > 0) {
+                dreier--;
+            } else if (addSchiff(x, y, 2, richtung) && zweier > 0) {
+                zweier--;
+            } else if (addSchiff(x, y, 1, richtung) && einer > 0) {
+                einer--;
+            }
+            versuche++;
+        } while ((dreier > 0 || zweier > 0 || einer > 0) && versuche < 1000);
+        return dreier == 0 && zweier == 0 && einer == 0;
+    }
+
     public Zustand getMapKarteTeil(int x, int y) {
-        if(!shipVisible && mapKarte[x][y] == Zustand.aliveShip) {
+        if (!shipVisible && mapKarte[x][y] == Zustand.aliveShip) {
             return Zustand.water;
         }
         return mapKarte[x][y];
